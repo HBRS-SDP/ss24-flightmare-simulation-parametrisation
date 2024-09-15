cfa6148b72ff675c57a4f45989b73ea4a961adf9
saloni pathak <badbit1000101@gmail.com>
2024-09-15 20:40:35 +0200

    added launch file for the setup pf fully parameterized flightmare simulation, Fixes #3

--- flightmare_simulation/config/default_config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-catkin build flightros -DBUILD_SAMPLES:=ON
-roslaunch flightros rotors_gazebo.launch
-gnome-terminal -- rostopic pub /hummingbird/wind_speed rotors_comm/WindSpeed "{header: {stamp: now}, velocity: {x: 0.0, y: 0.0, z: 0.0}}" -r 10
